6707d0dc7f4ce6b3a83f67e2b0a353fe63fc8910
Luiz Ferraz <luiz.ferraz@croct.com>
2023-09-21 15:12:09 -0300

    Re-export JSON operations with proper types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+import {JsonValue} from './mutable';
+import {ReadonlyJsonValue} from './readonly';
+
 export * from './mutable';
 export * from './readonly';
+
+export const typeSafeJsonParse: (jsonString: string) => JsonValue = JSON.parse;
+
+export const typedSafeStringify: (jsonValue: ReadonlyJsonValue) => string = JSON.stringify;
